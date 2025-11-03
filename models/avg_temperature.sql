b6eaeb70c7447c7a6a8957cddc2291b35ca3c05b
Kassandra ter Beek <kam.terbeek@gmail.com>
2025-11-03 09:05:27 +0000

    fix field for value1

--- models/avg_temperature.sql
+++ models/avg_temperature.sql
@@ -1,18 +1,6 @@
 {{ config(materialized='table') }}
 
-with tavg_data as (
-
-    select
-        s.id as station_id,
-        t.tavg
-    from {{ source('ghcnm', 'ghcnm_tavg') }} t
-    left join {{ source('ghcnm', 'ghcnm_tavg_stations') }} s
-    on t.id = s.id
-
-)
-
 select
-    station_id,
-    avg(tavg) as avg_temperature
-from tavg_data
-group by station_id
+    id as station_id,
+    value1 / 100.0 as avg_temperature
+from {{ source('ghcnm', 'ghcnm_tavg') }}
