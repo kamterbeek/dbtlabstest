86b49a83494d555a4ad04a35bd95afcb00b84d58
Kassandra ter Beek <kam.terbeek@gmail.com>
2025-11-03 08:38:37 +0000

    avg_temp

--- models/avg_temperature.sql
+++ models/avg_temperature.sql
@@ -1,9 +1,18 @@
 {{ config(materialized='table') }}
 
-SELECT
-  station_id,
-  AVG(value) AS avg_temperature
-FROM {{ source('ghcnm', 'ghcnm_tavg') }}
-GROUP BY station_id
+with tavg_data as (
 
+    select
+        s.id as station_id,
+        t.tavg
+    from {{ source('ghcnm', 'ghcnm_tavg') }} t
+    left join {{ source('ghcnm', 'ghcnm_tavg_stations') }} s
+    on t.id = s.id
 
+)
+
+select
+    station_id,
+    avg(tavg) as avg_temperature
+from tavg_data
+group by station_id
