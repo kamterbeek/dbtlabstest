dbb2a35f56a5848982fd9c8c6009c9edc6b0ea43
Kassandra ter Beek <kam.terbeek@gmail.com>
2026-02-04 07:23:38 +0000

    fixing jinja syntax

--- macros/jinja.sql
+++ macros/jinja.sql
@@ -1,18 +1,16 @@
-{# if and else#}
+{# if and else #}
 {% set temperature = 60.0 %}
 
 On a day like this, I especially like
 
 {% if temperature > 70 %}
-a refressing lemon sorbet
-
+a refreshing lemon sorbet
 {% else %}
 a decadent chocolate cake.
-
 {% endif %}
 
-{#for loops and variables#}
+{# for loops and variables #}
 
-{% for j in range(26)%}
-   select {{ j }} as number {% if not loop.last %} union all {% end if %}
-{% endfor %}  
+{% for j in range(26) %}
+select {{ j }} as number{% if not loop.last %} union all{% endif %}
+{% endfor %}
